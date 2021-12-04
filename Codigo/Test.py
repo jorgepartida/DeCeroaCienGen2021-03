89b08771c6dc7325446c93bb57b94f3c04e02355
jorgepartida <jorgepartida@localhost>
2021-12-04 10:17:45 -0600

    JP

--- Codigo/Test.py
+++ Codigo/Test.py
@@ -1,13 +1,21 @@
 
+import pandas as pd
+import numpy as np
+from scipy import stats
 
+x = np.array([12, 34, 67, 75, 91, 91, 132, 198])
 
+print(np.mean(x))
 
+print(np.median(x))
 
+print(stats.mode(x))
 
+print(x.max() - x.min())
 
+print(np.var(x))
 
-
-
+print(np.std(x))
 '''
 import numpy as np
 
@@ -29,8 +37,7 @@ print(norma)
 
 norma = np.linalg.norm(y)
 print(norma)
-'''
-'''
+
 x = np.array([[2,7,3,5],
               [4,5,2,12],
              [1,2,1,-2]])
@@ -56,7 +63,7 @@ import pandas as pd
 import numpy as np
 from scipy import stats
 
-x = np.array([10,11,13,21,17,16,9])
+x = np.array([9,3,8,8,9,8,9,18,3,7,9,1,0])
 
 print(np.mean(x))
 
@@ -69,9 +76,9 @@ print(x.max() - x.min())
 print(np.var(x))
 
 print(np.std(x))
+
+
+
 '''
-
-
-
 
 
